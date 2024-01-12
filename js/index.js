1383001a9b0208ec222fbf2515535f69ef35b1a9
Jana May <jana.ricarda.may@gmail.com>
2024-01-12 10:52:05 +0100

    solve challenge

--- js/index.js
+++ js/index.js
@@ -6,8 +6,18 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const newToast = document.createElement("li");
+  const toastMessage = document.createElement("p");
+
+  newToast.classList.add("toast-container__message");
+
+  toastMessage.textContent = "I'm a toast message.";
+
+  newToast.append(toastMessage);
+  toastContainer.append(newToast);
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.remove();
 });
